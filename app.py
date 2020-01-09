fd7ac8c881ef5ad6d9de77a1e3f2b8866cb01e91
Noreen Wu <noreenwu@gmail.com>
2020-01-09 15:45:33 -0500

    feat: new endpoints /cupcakes, /ingredients

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import os
-from flask import Flask
-from models import setup_db
+from flask import Flask, jsonify
+from models import *
 from flask_cors import CORS
+import json
+from sqlalchemy.exc import DatabaseError
 
 def create_app(test_config=None):
 
@@ -11,9 +13,9 @@ def create_app(test_config=None):
 
     @app.route('/')
     def get_greeting():
-        excited = os.environ['EXCITED']
+        # excited = os.environ['EXCITED']
         greeting = "Hello" 
-        if excited == 'true': greeting = greeting + "!!!!!"
+        # if excited == 'true': greeting = greeting + "!!!!!"
         return greeting
 
     @app.route('/coolkids')
@@ -21,8 +23,41 @@ def create_app(test_config=None):
         return "Be cool, man, be coooool! You're almost a FSND grad!"
 
     @app.route('/cupcakes')
-    def view_cupcakes():
-        return "Cupcakes!"
+    def get_cupcakes():
+        try:
+            cupcakes = Cupcake.query.all()
+        except DatabaseError:
+            abort(422)
+
+        print("cupcakes")
+        clist = []
+        for c in cupcakes:
+            clist.append(c.format())
+
+        return jsonify(clist)
+
+
+    @app.route('/ingredients')
+    def get_ingredients():
+        try:
+            ingredients = Ingredient.query.all()
+        except DatabaseError:
+            abort(422)
+        print("ingredients")
+        ilist = []
+        for i in ingredients:
+            ilist.append(i.format())
+
+        return jsonify(ilist)
+        
+
+    @app.errorhandler(422)
+    def cannot_process(error):
+        return jsonify({
+            "success": False,
+            "error": 422,
+            "message": "Not processable"
+        }), 422
 
     return app
 
