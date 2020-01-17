1518f3d5ffadcc8fa98f409eb159a01240dfe4ac
Noreen Wu <noreenwu@gmail.com>
2020-01-16 21:47:59 -0500

    feat: additional tests on endpoints

--- test_cupcake.py
+++ test_cupcake.py
@@ -34,7 +34,9 @@ class CupcakeTests(unittest.TestCase):
         """Executed after reach test"""
         pass
 
+    # test getting cupcakes
     def test_get_cupcakes(self):
+        print ("test GET cupcakes")
         res = self.client().get('/cupcakes')
         data = json.loads(res.data)
 
@@ -42,58 +44,130 @@ class CupcakeTests(unittest.TestCase):
         self.assertEqual(data['success'], True)
         self.assertTrue(data['cupcakes'])
 
+    # test getting specific cupcake
     def test_get_specific_cupcake(self):
-        res = self.client().get('/cupcakes/9')
+        print ("test GET specific cupcake")
+        res = self.client().get('/cupcakes/1')
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
         self.assertTrue(data['cupcakes'])
 
+    # test getting a non-existent cupcake returns 404
+    def test_get_non_existent_cupcake(self):
+        print ("test GET non-existent cupcake returns 404")
+        res = self.client().get('cupcakes/999')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+
+
+    # test creation of new cupcake
     def test_create_cupcake(self):
         res = (
             self.client()
                 .post('/cupcakes',
-                      json={'name': 'Chocolate Mocha Cream 119',
-                            'description': 'intense combination of coffee and dark chocolate',
-                            'ingredients': [{'kind': 'topping', 'name': 'chips'}]
+                      json={'name': 'New Cupcake',
+                            'description': 'description of New Cupcake',
+                            'ingredients': [{'kind': 'topping', 'name': 'new topping'}]
                       })
         )
         data = json.loads(res.data)
         # verify that it was created in the database
         the_cupcake = (Cupcake.query
-                              .filter_by(name='Chocolate Mocha Cream 119')  
-                              .filter_by(description='intense combination of coffee and dark chocolate')
+                              .filter_by(name='New Cupcake')  
+                              .filter_by(description='description of New Cupcake')
                               .first()
         )
         self.assertTrue(the_cupcake is not None)
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
+        self.assertTrue(data['cupcakes'])
 
 
+    # test creation of new cupcake with faulty input returns 400, bad request:
+    #   kind is missing from ingredients list
+    def test_create_cupcake_faulty_input(self):
+        res = (
+            self.client()
+                .post('/cupcakes',
+                      json={'name': 'New Cupcake Faulty',
+                            'description': 'description of New Cupcake Faulty',
+                            'ingredients': [{'name': 'new topping for faulty cupcake'}]
+                      })
+        )
+        data = json.loads(res.data)
+        # verify that it was not created in the database
+        the_cupcake = (Cupcake.query
+                              .filter_by(name='New Cupcake Faulty')  
+                              .filter_by(description='description of New Cupcake Faulty')
+                              .one_or_none()
+        )
+        self.assertTrue(the_cupcake is None)
+        self.assertEqual(res.status_code, 400)
+        self.assertEqual(data['success'], False)
+
+
+    # test creation of new cupcake with faulty input returns 400, bad request:
+    #   name is missing
+    def test_create_cupcake_faulty_input(self):
+        print ("test create cupcake with faulty input, no name specified")
+        res = (
+            self.client()
+                .post('/cupcakes',
+                      json={'description': 'description of New Cupcake Faulty Name Missing',                            
+                            'ingredients': [{'kind': 'topping', 'name': 'new topping for faulty cupcake'}]
+                      })
+        )
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 400)
+        self.assertEqual(data['success'], False)
+
+
+    # test deleting a cupcake, and check that it was really deleted
     def test_delete_cupcake(self):
-        res = self.client().delete('/cupcakes/23')
+        res = self.client().delete('/cupcakes/2')
         data = json.loads(res.data)
 
-        cupcake = Cupcake.query.filter(Cupcake.id == 23).one_or_none()
+        cupcake = Cupcake.query.filter(Cupcake.id == 2).one_or_none()
 
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
         self.assertEqual(cupcake, None)
 
+    # test deleting a non-existent cupcake returns 404
+    def test_delete_non_existent_cupcake(self):
+        res = self.client().delete('/cupcakes/222')
+        data = json.loads(res.data)
 
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+
+
+    # test updating a cupcake; changing its name and description
     def test_update_cupcake(self):
-        res = self.client().patch('/cupcakes/9', 
-                                  json = {"name": "HALLOW", "description": "new", "ingredients": [{"name": "carrot", "kind": "cake"}] })
+        res = self.client().patch('/cupcakes/3', 
+                                  json = {"name": "change of name", "description": "new description", "ingredients": [] })
 
         data = json.loads(res.data)
         the_cupcake = (Cupcake.query
-                              .filter(Cupcake.id==9)
+                              .filter(Cupcake.id==3)
                               .first()
         )
-        self.assertEqual(the_cupcake.short()['name'], 'HALLOW')
+        self.assertEqual(the_cupcake.short()['name'], 'change of name')
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
+
+    # test updating a non-existent cupcake results in 404
+    def test_update_non_existent_cupcake(self):
+        res = self.client().patch('/cupcakes/300', 
+                                  json = {"name": "change of name", "description": "new description", "ingredients": [] })
+
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)                                  
 
 
 
